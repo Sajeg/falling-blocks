c53baf9eb1ab15a5e2995c74a5063412bb3d6580
Sajeg <jfx@posteo.de>
2023-04-15 22:12:44 +0200

    added new tutorial (not working)

--- Scripts/Tutorial.gd
+++ Scripts/Tutorial.gd
@@ -1,55 +1,70 @@
 extends Control
 
+var pos1_pressed = false
+var pos2_pressed = false
+var pos3_pressed = false
+
+var anim_finished = false
 
 func _ready():
-	$Blue/AnimationPlayer.play("Idle")
-	$Pink/AnimationPlayer.play("idle")
-	$Green/AnimationPlayer.play("idle")
-	$Text/RichTextLabel.visible = true
-	var rand = randi()%3
-	if rand == 0:
-		$Player/BlockBlue.visible = true
-	if rand == 1:
-		$Player/BlockGreen.visible = true
-	if rand == 2:
-		$Player/BlockPink.visible = true
+	$Blocks/Player/BlockBlue.modulate = Color("82B7E8")
+	$Blocks/Blue/BlockBlue.modulate = Color("82B7E8")
+	$Blocks/Green/BlockGreen.modulate = Color("AEE881")
+	$Blocks/Pink/BlockPink.modulate = Color("DB81E8")
+	$AnimationPlayer.play("tutorial")
+	yield($AnimationPlayer, "animation_finished")
+	anim_finished = true
 
 
-func _on_TouchScreenButton_pressed():
-	$Text/weiter2.visible = true
-	$Text/TouchScreenButton.visible = false
-	$Text/RichTextLabel.visible = false
-	$Text/Label3.visible = true
-	$arrow.visible = true
-	$arrow2.visible = true
-	$Label.visible = true
-	$Label2.visible = true
+func _process(delta):
+	if $Blocks/Blue.visible:
+		$Blocks/Blue.position.y += 1
+		if $Blocks/Blue.position.y > 1800:
+			$Blocks/Blue.position.x = 310
+			$Blocks/Blue.position.y = -100
+	if $Blocks/Pink.visible:
+		$Blocks/Pink.position.y += 1
+		if $Blocks/Blue.position.y > 1800:
+			get_tree().change_scene("res://Scenes/Main.tscn")
+	if $Blocks/Green.visible:
+		$Blocks/Green.position.y += 1
 
 
-func _on_weiter2_pressed():
-	$Blue/AnimationPlayer.play("special")
-	$Pink/AnimationPlayer.play("special")
-	$Green/AnimationPlayer.play("special")
-	$Text/startSpiel.visible = true
-	$Text/RichTextLabel2.visible = true
-	$Label3.visible = true
-	$Text/Label3.visible = false
-	$arrow.visible = false
-	$arrow2.visible = false
-	$Label.visible = false
-	$Label2.visible = false
-
-
-func _on_startSpiel_pressed():
-	get_tree().change_scene("res://Scenes/Game.tscn")
+func next_animation():
+	$AnimationPlayer.play("tutorial_2")
+	yield($AnimationPlayer, "animation_finished")
+	$Blocks/Blue.visible = true
+	$Blocks/Blue.position.x = 310
+	$Blocks/Blue.position.y = -100
 
 
 func _on_pos1_pressed():
-	$Player.position.x = 100
+	$Blocks/Player.position.x = 100
+	pos1_pressed = true
+	if pos1_pressed and pos2_pressed and pos3_pressed and anim_finished:
+		anim_finished = false
+		next_animation()
+
 
 func _on_pos2_pressed():
-	$Player.position.x = 310
+	$Blocks/Player.position.x = 310
+	pos2_pressed = true
+	if pos1_pressed and pos2_pressed and pos3_pressed and anim_finished:
+		anim_finished = false
+		next_animation()
 
 
 func _on_pos3_pressed():
-	$Player.position.x = 520
+	$Blocks/Player.position.x = 520
+	pos3_pressed = true
+	if pos1_pressed and pos2_pressed and pos3_pressed and anim_finished:
+		anim_finished = false
+		next_animation()
+
+
+func _on_Area2D_area_entered(area):
+	if area.name == "Blue":
+		$Blocks/Blue.visible = false
+		$AnimationPlayer.play("tutorial_3")
+	if area.name == "Pink" or "Green":
+		$AnimationPlayer.play("tutorial_3")
